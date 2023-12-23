ca634c83253cd36695231da2fd47d59971020984
Klotzi111 <Klotzi111@users.noreply.github.com>
2023-12-23 18:15:09 +0100

    initial commit

---
+++ src/main/java/GradleRobot/GradleRobot.java
@@ -0,0 +1,63 @@
+package GradleRobot;
+
+// import java.awt.Color;
+import robocode.HitByBulletEvent;
+import robocode.HitWallEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * GradleRobot - a robot by (your name here)
+ */
+public class GradleRobot extends Robot {
+	/**
+	 * run: GradleRobot's default behavior
+	 */
+	@Override
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while (true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	@Override
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	@Override
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	@Override
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}
+}
